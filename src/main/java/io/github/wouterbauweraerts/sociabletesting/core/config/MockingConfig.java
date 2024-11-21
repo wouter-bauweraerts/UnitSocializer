140362f027bb1856eccbe64606caf740d9fd5754
Wouter Bauweraerts <wouter.bauweraerts@telenet.be>
2024-11-21 13:12:07 +0100

    Cleanup obsolete annotations in MockingConfig

--- src/main/java/io/github/wouterbauweraerts/sociabletesting/core/config/MockingConfig.java
+++ src/main/java/io/github/wouterbauweraerts/sociabletesting/core/config/MockingConfig.java
@@ -3,17 +3,9 @@ package io.github.wouterbauweraerts.sociabletesting.core.config;
 import java.lang.annotation.Annotation;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonRootName;
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.annotation.Nulls;
-
-@JsonRootName("sociable-test")
 public record MockingConfig(
-        @JsonSetter(nulls= Nulls.SKIP, contentNulls = Nulls.FAIL)
         List<Class<? extends Annotation>> annotations,
-        @JsonSetter(nulls= Nulls.SKIP, contentNulls = Nulls.FAIL)
         List<Class<?>> classes,
-        @JsonSetter(nulls= Nulls.SKIP, contentNulls = Nulls.FAIL)
         List<String> packages
 ) {
 }
