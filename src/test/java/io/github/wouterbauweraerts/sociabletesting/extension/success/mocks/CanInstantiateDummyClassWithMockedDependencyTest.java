e2bc89cd1a6f1aae0bfaab0b7fc926d7e309428f
Wouter Bauweraerts <wouter.bauweraerts@telenet.be>
2024-11-21 18:18:35 +0100

    Restructure some tests + remove .gitkeep

--- src/test/java/io/github/wouterbauweraerts/sociabletesting/extension/success/mocks/CanInstantiateDummyClassWithMockedDependencyTest.java
+++ src/test/java/io/github/wouterbauweraerts/sociabletesting/extension/success/mocks/CanInstantiateDummyClassWithMockedDependencyTest.java
@@ -1,4 +1,4 @@
-package io.github.wouterbauweraerts.sociabletesting.extension.success;
+package io.github.wouterbauweraerts.sociabletesting.extension.success.mocks;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mockingDetails;
