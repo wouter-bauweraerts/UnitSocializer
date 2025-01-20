36fb310297f926156d1216180ffc265d9cd1c603
Wouter Bauweraerts <wouter.bauweraerts@telenet.be>
2025-01-20 07:30:54 +0100

    Javadoc for Resolve annotation

--- unit-socializer-core/src/main/java/io/github/wouterbauweraerts/unitsocializer/core/annotations/Resolve.java
+++ unit-socializer-core/src/main/java/io/github/wouterbauweraerts/unitsocializer/core/annotations/Resolve.java
@@ -6,9 +6,31 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation used to define a resolution strategy for a specific type.
+ * <p>
+ * This annotation allows developers to specify the resolution of one class
+ * using another class, typically for dependency injection or replacement usage.
+ *
+ * @author Wouter Bauweraerts
+ * @version 0.0.1
+ * @since jan 19 2025
+ */
 @Target(ANNOTATION_TYPE)
 @Retention(RUNTIME)
 public @interface Resolve {
+
+    /**
+     * Specifies the class type that this resolution applies to.
+     *
+     * @return the target class type that will be resolved.
+     */
     Class<?> forClass();
+
+    /**
+     * Specifies the class type to be used for resolution.
+     *
+     * @return the class type that will replace or be used for the resolution.
+     */
     Class<?> use();
 }
