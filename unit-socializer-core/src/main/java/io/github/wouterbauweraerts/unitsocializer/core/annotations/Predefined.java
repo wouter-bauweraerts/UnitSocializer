31a94aa237cb9982f95f1f6709b78f9d6ef5b67a
Wouter Bauweraerts <wouter.bauweraerts@telenet.be>
2025-01-20 07:34:43 +0100

    Add javadoc for Predefined and InjectTestInstance annotations

--- unit-socializer-core/src/main/java/io/github/wouterbauweraerts/unitsocializer/core/annotations/Predefined.java
+++ unit-socializer-core/src/main/java/io/github/wouterbauweraerts/unitsocializer/core/annotations/Predefined.java
@@ -8,6 +8,14 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * The {@code Predefined} annotation can be applied to fields to indicate
+ * that the field value must be used to resolve fields of the given type.
+ * 
+ * @author Wouter Bauweraerts
+ * @since nov 21 2024
+ * @version 0.0.1
+ */
 @Documented
 @Target(FIELD)
 @Retention(RUNTIME)
